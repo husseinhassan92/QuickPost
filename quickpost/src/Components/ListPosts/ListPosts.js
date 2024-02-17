388dcb15d8d5ff6f95242a5ccc154af28ef48565
husseinhassan92 <hussein.hassan2020@outlook.com>
2024-02-17 13:13:25 +0200

    adding axios function

--- quickpost/src/Components/ListPosts/ListPosts.js
+++ quickpost/src/Components/ListPosts/ListPosts.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const ListPosts = () => {
+  useEffect(() => {
+    axios.get("https://dummyapi.io/data/v1/post?page=1&limit=10",{
+      headers:{
+        "app-id":"65d08a4661de33117cf6503f"
+      }
+    })
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => console.log('Error:', err))
+  },[])
   return (
     <div>ListPosts</div>
   )
