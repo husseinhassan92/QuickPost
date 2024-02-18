bfd14bf72a46e12485de9a4fe7f511ec06e93be6
salahiti <salahmohmed504@gmail.com>
2024-02-18 20:09:36 +0200

    first

--- quickpost/src/App.js
+++ quickpost/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
-import Posts from './Components/Posts/Posts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Welcom from './Pages/WelcomePage/Welcom';
+import { BrowserRouter as Router, Route, Switch , Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import Login from './Pages/Login/Login';
+import Sginup from './Pages/SginUp/SginUp';
+
+
 
 function App() {
   return (
-    <Posts/>
-  );
+    <>
+
+
+    <Router>
+      <Switch>
+      <Route exact path='/' component={Welcom} />
+      <Route path='/Login' component={Login} />
+      <Route path='/Sginup'  component={Sginup}/>
+
+      </Switch>
+    </Router>
+
+   
+    
+   
+
+  
+    </>
+    );
 }
 
 export default App;
